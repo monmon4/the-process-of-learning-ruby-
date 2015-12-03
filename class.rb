3a68c4dfe7126571fa5b15160dc34851cfff6275
Mona Ahmed <mona_ahmed_94@hotmail.com>
2015-12-03 11:08:38 -0800

    adding automatic getter and setter

--- class.rb
+++ class.rb
@@ -15,3 +15,15 @@ person1 = Person.new("joe", 14)
 puts person1.name #=>joe
 person1.name = "Mike"
 puts person1.name #=>Mike
+
+
+class Personauto
+	attr_accessor :name, :age #geters and setters automatically
+end
+
+person2 = Personauto.new
+puts person2.name #=>nil
+person2.name = "Mona"
+person2.age = 21
+puts person2.name
+puts person2.age
