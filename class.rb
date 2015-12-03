602aa78b8a7c25e6cc6aad91078fe5325e5d87dd
Mona Ahmed <mona_ahmed_94@hotmail.com>
2015-12-03 11:22:34 -0800

    self to have a better control

--- class.rb
+++ class.rb
@@ -18,12 +18,21 @@ puts person1.name #=>Mike
 
 
 class Personauto
-	attr_accessor :name, :age #geters and setters automatically
+	attr_accessor :name #geters and setters automatically
+	attr_reader :age #only getter cause i want to have better control on setter
+
+	def initialize (name, ageVar) #constructor
+		@name = name
+		self.age = ageVar #calls the age method below
+	end
+	def age= (new_age)
+		@age = new_age unless new_age > 120 #to check the age first before setting it
+
+	end
+
 end
 
-person2 = Personauto.new
-puts person2.name #=>nil
-person2.name = "Mona"
-person2.age = 21
-puts person2.name
-puts person2.age
+person2 = Personauto.new("kim", 13)
+puts "my age is #{person2.age}" #=> my age is 13
+person2.age = 130 #me4 hatet3adel 3ala4an unless elly fo2
+puts "my age is #{person2.age}" #=> my age is 13
